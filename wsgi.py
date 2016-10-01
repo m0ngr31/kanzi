d8d2e8bc25d4682d2d383f894d6207c5a8d7d5fb
Joe Ipson <joeipson@gmail.com>
2016-09-30 22:35:14 -0600

    Removed unneeded function

--- wsgi.py
+++ wsgi.py
@@ -791,19 +791,10 @@ HANDLERS = [
 
 # The main entry point for lambda
 def lambda_handler(event, context):
-  if event['session']['new']:
-    on_session_started({'requestId': event['request']['requestId']}, event['session'])
-
   if event['request']['type'] == "LaunchRequest":
     return on_launch(event['request'], event['session'])
   elif event['request']['type'] == "IntentRequest":
     return on_intent(event['request'], event['session'])
-
-def on_session_started(session_started_request, session):
-    """ Called when the session starts """
-
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
 
 def on_launch(launch_request, session):
     return prepare_help_message()
