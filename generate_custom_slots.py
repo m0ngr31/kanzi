b45114af06f5f339b2a70d5a26edea79a1725b01
jingai <jingai@floatingpenguins.com>
2017-05-12 01:39:44 -0400

    Fix slot generator.

--- generate_custom_slots.py
+++ generate_custom_slots.py
@@ -1,7 +1,8 @@
-from kodi import Kodi
 import re
 import string
 import random
+import os
+from kodi_voice import KodiConfigParser, Kodi
 
 config_file = os.path.join(os.path.dirname(__file__), "kodi.config")
 config = KodiConfigParser(config_file)
