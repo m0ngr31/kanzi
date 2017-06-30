d1d9aa0ac3c6b373bcce01ec1e1501be6cee97bd
jingai <jingai@floatingpenguins.com>
2017-06-30 03:41:26 -0400

    Evenly distribute string lengths from slot generator.

--- generate_custom_slots.py
+++ generate_custom_slots.py
@@ -10,24 +10,63 @@ config = KodiConfigParser(config_file)
 kodi = Kodi(config)
 
 
+def most_words(l=[]):
+  longest = 0
+  for s in l:
+    if len(s.split()) > longest:
+      longest = len(s.split())
+  return longest
+
+
+def sort_by_words(l, longest):
+  distributed = []
+  for i in range(1, longest + 1):
+    dl = [s for s in l if len(s.split()) == i]
+    if len(dl) > 0:
+      distributed.append(dl)
+  return distributed
+
+
 def clean_results(resp, cat, key, limit=100):
-  all = []
+  cleaned = []
   if 'result' in resp and cat in resp['result']:
     for v in retrieved['result'][cat]:
       name = kodi.sanitize_name(v[key], normalize=False)
       # omit titles with digits, as Amazon never passes numbers as digits
       if not re.search(r'\d', name):
-        all.append(name)
+        cleaned.append(name)
 
-  cleaned = list(set(all))
+  cleaned = list(set(cleaned))
   cleaned = filter(None, cleaned)
   random.shuffle(cleaned)
-  cleaned = cleaned[:limit]
 
-  return cleaned
+  # distribute strings evenly by number of words
+  if len(cleaned) > limit:
+    longest = most_words(cleaned)
+    distributed = sort_by_words(cleaned, longest)
+    if len(distributed) > 0:
+      total = 0
+      cleaned = []
+      while total < limit:
+        for l in distributed:
+          if len(l) > 0:
+            total += 1
+            cleaned.append(l.pop())
+
+  # sort by number of words just for visibility
+  if len(cleaned) > 0:
+    longest = most_words(cleaned)
+    distributed = sort_by_words(cleaned, longest)
+    if len(distributed) > 0:
+      cleaned = []
+      for dl in distributed:
+        cleaned += [l for l in dl]
+
+  return cleaned[:limit]
 
 
 def write_file(filename, items=[]):
+  print 'Writing: %s' % (filename)
   f = open(filename, 'w')
   for a in items:
     f.write("%s\n" % a.encode("utf-8"))
